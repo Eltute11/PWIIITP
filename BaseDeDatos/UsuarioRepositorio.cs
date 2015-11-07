fd3714858fa7e770076f4c20581e435e7219ed3e
jurcola <juanig.urcola@gmail.com>
2015-11-07 19:47:12 -0300

    Code Behind Modificacion
    Falto agregar esto al ultimo commit

--- BaseDeDatos/UsuarioRepositorio.cs
+++ BaseDeDatos/UsuarioRepositorio.cs
@@ -7,7 +7,7 @@ using BaseDeDatos.Modelo;
 
 namespace BaseDeDatos
 {
-    public class UsuarioRepositorio 
+    public class UsuarioRepositorio
     {
         private MaratonEntities Contexto { get; set; }
 
@@ -19,7 +19,6 @@ namespace BaseDeDatos
         public int Crear(Usuario usuario)
         {
             Contexto.Usuario.Add(usuario);
-
             Contexto.SaveChanges();
 
             return usuario.ID;
@@ -34,9 +33,25 @@ namespace BaseDeDatos
             return _usuario;
         }
 
+        public void ModificarDatosUsuario (int usuarioID, string Nombre, string Apellido, string Fecha_Nacimiento, string Residencia) 
+        {
+            var _usuario = (from a in Contexto.Usuario
+                            where a.ID == usuarioID
+                            select a).FirstOrDefault();
+
+            _usuario.Nombre = Nombre;
+            _usuario.Apellido = Apellido;
+            _usuario.Fecha_Nacimiento = DateTime.Parse(Fecha_Nacimiento);
+            _usuario.Residencia = Residencia;
+
+            Contexto.SaveChanges();
+
+        }
+
+
         public bool Actualizar(Modelo.Usuario usuario)
         {
-           // Contexto.Usuario.
+            // Contexto.Usuario.
 
             return true;
         }
