0e591c306f0ac396b7bef7ec69e7404b24cd1fbb
jurcola <juanig.urcola@gmail.com>
2015-11-07 19:42:32 -0300

    Ajustes Grilla usuario pendientes y modificacion datos de datos personales
    Se vuelve agregar las modificaciones encargadas de llenar grilla
    pendientes y update de datos personales del usuario,

--- Presentacion/GrupoUsuario/MisMaratones.aspx.cs
+++ Presentacion/GrupoUsuario/MisMaratones.aspx.cs
@@ -12,11 +12,12 @@ namespace Presentacion.GrupoUsuario
     {
         protected void Page_Load(object sender, EventArgs e)
         {
-            Usuario usuario = new Usuario();
-
-            usuario = (Usuario) Session["Usuario"];
-
+           Usuario usuario = new Usuario();
+           usuario = (Usuario) Session["Usuario"];
+           sdsUsuarioMaratonesPendiente.SelectCommand = "SELECT A.Nombre, A.Fecha, A.Km, A.ID FROM Maraton AS A INNER JOIN Maraton_Usuario AS B ON A.ID = B.MaratonID WHERE (B.UsuarioID = " + usuario.ID + ")";
             sdsUsuarioMaratonesHistorial.SelectCommand = "select d.Nombre, d.Lugar_Salida, b.Presente, b.Posicion, b.Abandono, b.Tiempo_Llegada, case b.Posicion when 1 then d.Premio_Uno when 2 then d.Premio_Dos when 3 then d.Premio_Tres else 0 end 'Premio' from Usuario a inner join Maraton_Usuario b on a.ID = b.UsuarioID inner join Maraton d on d.ID = b.MaratonID where (b.UsuarioID = " + usuario.ID + ")";
         }
+
+        
     }
 }
--- Presentacion/GrupoUsuario/UsuarioAdminDatos.aspx.cs
+++ Presentacion/GrupoUsuario/UsuarioAdminDatos.aspx.cs
@@ -4,18 +4,40 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
-
+using BaseDeDatos;
+using BaseDeDatos.Modelo;
 namespace Presentacion.GrupoUsuario
 {
     public partial class UsuarioAdminDatos : System.Web.UI.Page
     {
         protected void Page_Load(object sender, EventArgs e)
         {
+            if (!IsPostBack) 
+            {
+                Usuario usuario = new Usuario(); // Lo obtengo de BaseDeDatos.Modelo.Usuario
+                usuario = (Usuario)Session["Usuario"];
 
+                txtNombre.Text = usuario.Nombre;
+                txtApellido.Text = usuario.Apellido;
+                txtFechaNacimiento.Text = Convert.ToString(usuario.Fecha_Nacimiento).Substring(0, 10);  
+                txtResidencia.Text = usuario.Residencia;
+       
+            }
         }
 
         protected void btnModificar_Click(object sender, EventArgs e)
         {
+            var usuarioRepo = new UsuarioRepositorio();
+            Usuario usuarioModificar = new Usuario();
+            usuarioModificar = (Usuario)Session["Usuario"];
+
+            int usuarioID = usuarioModificar.ID;
+
+            usuarioRepo.ModificarDatosUsuario(usuarioID, txtNombre.Text, txtApellido.Text, txtFechaNacimiento.Text, txtResidencia.Text);
+
+
+
+
 
         }
     }
