71a1af7b93fce607432970a3d27260ec318ae97f
Matias <matias_araus@hotmail.com>
2015-11-15 13:56:40 -0300

    Consultas Linq
    Se crean las consultas:
    *ObtenerMaratonesPendientes [ADMIN]
    *ObtenerPosiciones [HOME]

--- BaseDeDatos/MaratonRepositorio.cs
+++ BaseDeDatos/MaratonRepositorio.cs
@@ -7,7 +7,7 @@ using BaseDeDatos;
 using BaseDeDatos.Modelo;
 using System.Data.Objects;
 using System.Collections;
-
+using System.Data.Objects.SqlClient;
 
 namespace BaseDeDatos
 {
@@ -85,11 +85,50 @@ namespace BaseDeDatos
             Contexto.SaveChanges();
         }
 
-        public object ObtenerRealizadasUsuario(int usuarioID)
+        public  object ObtenerMaratonesPendientes()
+        {
+            var maratonesPendientes = (from mu in Contexto.Maraton_Usuario
+                                       group new { mu.Maraton, mu } by new
+                                       {
+                                           ID = mu.Maraton.ID,
+                                           Nombre = mu.Maraton.Nombre,
+                                           Fecha = mu.Maraton.Fecha,
+                                           Km = mu.Maraton.Km,
+                                           Cant_Participantes = mu.Maraton.Cant_Participantes,
+                                           Premio_Uno = mu.Maraton.Premio_Uno,
+                                           Premio_Dos = mu.Maraton.Premio_Dos,
+                                           Premio_Tres = mu.Maraton.Premio_Tres,
+                                           mu.MaratonID
+                                       } into g
+                                       select new
+                                       {
+                                           Nombre_de_Maraton = g.Key.Nombre,
+                                           Fecha = g.Key.Fecha,
+                                           Km = g.Key.Km,
+                                           Esperados = g.Key.Cant_Participantes,
+                                           Inscriptos = g.Count(p => p.mu.UsuarioID != null),
+                                           Espera =
+                                           (from mu2 in Contexto.Maraton_Usuario
+                                            where
+                                            g.Key.MaratonID == mu2.MaratonID &&
+                                            mu2.Lista_Espera == true
+                                            select new
+                                            {
+                                                mu2.UsuarioID
+                                            }).Count(p => p.UsuarioID != null),
+                                           Premio_N1 = g.Key.Premio_Uno,
+                                           Premio_N2 = g.Key.Premio_Dos,
+                                           Premio_N3 = g.Key.Premio_Tres
+                                       });
+
+            return maratonesPendientes.ToList();
+        }
+
+        public object ObtenerHistorialUsuario(Usuario usuario)
         {
             var usuarioHistorial = (from b in Contexto.Maraton_Usuario
                                     where
-                                      b.UsuarioID == usuarioID
+                                      b.UsuarioID == usuario.ID
                                       && b.Maraton.Fecha < DateTime.Now
                                     select new
                                     {
@@ -99,11 +138,27 @@ namespace BaseDeDatos
                                         b.Posicion,
                                         b.Abandono,
                                         b.Tiempo_Llegada,
-                                        Premio = b.Posicion == 1 ? b.Maraton.Premio_Uno :
-                                                    b.Posicion == 2 ? b.Maraton.Premio_Dos :
+                                        Premio =    b.Posicion == 1 ? b.Maraton.Premio_Uno : 
+                                                    b.Posicion == 2 ? b.Maraton.Premio_Dos : 
                                                     b.Posicion == 3 ? b.Maraton.Premio_Tres : 0
-                                    });
-             return usuarioHistorial.ToList();
+                                    }).ToList();
+            return usuarioHistorial;
+        }
+
+        public object ObtenerPosiciones()
+        {
+            var maximo = Contexto.Maraton.Max(m => m.Fecha);
+
+            var posiciones = (from mu in Contexto.Maraton_Usuario
+                              join u in Contexto.Usuario on mu.UsuarioID equals u.ID
+                              join m in Contexto.Maraton on mu.MaratonID equals m.ID
+                              where maximo.Equals(m.Fecha)
+                              select new
+                              {
+                                  Posicion = SqlFunctions.StringConvert((double)mu.Posicion) + " " + u.Nombre + " " + u.Apellido
+                              }).ToList();
+
+            return posiciones;
         }
     }
 }
